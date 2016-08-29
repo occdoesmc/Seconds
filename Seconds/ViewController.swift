80bb218a2090cb5016c7fcf3e96b5844d4797249
Orlando Cossia Castiglioni <occ.github@hairesis.com>
2016-08-29 18:32:20 +0200

    Fix status bar appearance

--- Seconds/ViewController.swift
+++ Seconds/ViewController.swift
@@ -4,4 +4,8 @@ class ViewController: UIViewController {
     
     let intervals: Set = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
 }
