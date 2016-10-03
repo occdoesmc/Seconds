6820d638400c3e01c01cf7a13306f1bfb1f385f5
Orlando Castiglioni <occ.github@hairesis.com>
2016-10-03 17:13:27 +0200

    Add segue from animal list to interval list

--- Seconds/AnimalListViewController.swift
+++ Seconds/AnimalListViewController.swift
@@ -3,6 +3,13 @@ import UIKit
 class AnimalListViewController: UITableViewController {
     
     let animalList = AnimalList()
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        let animalIndex = tableView.indexPathForSelectedRow!.row
+        let animal = animalList.animalsByTime[animalIndex]
+        segue.destination.title = animal.name
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animalList.animalsByTime.count
