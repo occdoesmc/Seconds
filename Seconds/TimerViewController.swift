10985167b629626e276d77ebed0776cec085b90a
Orlando Castiglioni <occ.github@hairesis.com>
2016-10-10 18:31:05 +0200

    Implement timer functionality

--- Seconds/TimerViewController.swift
+++ Seconds/TimerViewController.swift
@@ -2,13 +2,38 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
-    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet private var timeLabel: UILabel!
     
     var seconds = 0
     var timeMultiplier = 0.0
     
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTimeLabel()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        timer = Timer.scheduledTimer(withTimeInterval: 1 / timeMultiplier, repeats: true, block: timerDidFire)
+    }
+    
+    private func timerDidFire(timer: Timer) {
+        seconds -= 1
+        updateTimeLabel()
+        if seconds == 0 {
+            timer.invalidate()
+            let timerDoneTitle = NSLocalizedString("Timer Done", comment: "")
+            let timerDoneController = UIAlertController(title: timerDoneTitle, message: nil, preferredStyle: .alert)
+            let okeyTitle = NSLocalizedString("Okay", comment: "")
+            let okayAction = UIAlertAction(title: okeyTitle, style: .default, handler: nil)
+            timerDoneController.addAction(okayAction)
+            present(timerDoneController, animated: true, completion: nil)
+        }
+    }
+    
+    private func updateTimeLabel() {
         timeLabel.text = String(seconds)
     }
 
